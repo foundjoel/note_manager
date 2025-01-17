a227c602e16646bdea6f1f7e725dbfddc7186096
foundjoel <kiseliova.marina2018@yandex.ru>
2025-01-17 22:27:51 +0300

    check_deadline.py

--- check_deadline.py
+++ check_deadline.py
@@ -1,41 +1,4 @@
 from datetime import datetime
-
-print('Добро пожаловать в менеджер заметок! Вы можете добавить новую заметку.')
-
-current_date = datetime.now().date()
-print('Сегодняшняя дата:', current_date.strftime("%d-%m-%Y"))
-
-username = input('Введите ваше имя:')
-print('Имя пользователя:', username)
-
-titles = []
-while True:
-    header = input("Введите заголовок или 'стоп' для завершения: ")
-    if header.lower() == 'стоп':
-        break
-    titles.append(header)
-print('\nСписок заголовков: ')
-for idx, title in enumerate(titles, start=1):
-    print(f"{idx}. {title}")
-
-content = input('Введите описание заметки:')
-print('Описание заметки:', content)
-
-statuses = []
-current_status = 'В процессе'
-print(f'Текущий статус заметки: {current_status}')
-possible_statuses = ['выполнено', 'в процессе', 'отложено']
-print("Доступные статусы: " + ", ".join(possible_statuses))
-while True:
-    new_status = input('Введите новый статус заметки: ')
-    if new_status in possible_statuses:
-        current_status = new_status
-        break
-    else:
-        print('Некорректный статус. Пожалуйста, выберите один из доступных статусов.')
-print(f'\nОбновлённый статус заметки: {current_status}')
-
-from datetime import datetime
 while True:
     try:
         deadline_str = input("Введите дату дедлайна в формате дд-мм-гггг: ")
@@ -55,14 +18,3 @@ while True:
     except Exception as e:
         print(f"Произошла непредвиденная ошибка: {str(e)}")
         print("Пожалуйста, попробуйте снова.")
-
-note = [
-    ('Имя пользователя:', username),
-    ('Заголовок:', titles),
-    ('Описание:', content),
-    ('Статус:', current_status),
-    ('Дата дедлайна:', days_difference)
-]
-print('\nИнформация о заметке: ' )
-for index, about_note in enumerate(note, start=1):
-    print(f"{index}. {about_note}")
