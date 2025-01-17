b786f89a91acdc703a2e349646268fc0d32f2636
foundjoel <kiseliova.marina2018@yandex.ru>
2025-01-17 22:26:09 +0300

    update_status.py

--- update_status.py
+++ update_status.py
@@ -1,19 +1,3 @@
-username = input('Введите ваше имя:')
-print('Имя пользователя:', username)
-
-title = []
-while True:
-    header = input("Введите заголовок (или 'стоп' для завершения):")
-    if header.lower() == 'стоп':
-        break
-    title.append(header)
-print("\nСписок заголовков:")
-for idx, h in enumerate(title, start=1):
-    print(f"{idx}. {h}")
-
-content = input('Введите описание заметки:')
-print('Описание заметки:', content)
-
 statuses = []
 current_status = 'В процессе'
 print(f'Текущий статус заметки: {current_status}')
@@ -27,23 +11,3 @@ while True:
     else:
         print('Некорректный статус. Пожалуйста, выберите один из доступных статусов.')
 print(f'\nОбновлённый статус заметки: {current_status}')
-
-created_date = int(input('Введите день заметки:'))
-created_month = int(input('Введите месяц заметки:'))
-print('Дата заметки:', f'День: {created_date}, Месяц: {created_month}')
-
-issue_date = int(input('Введите дату дедлайна:'))
-issue_month = int(input('Введите месяц заметки:'))
-print('Дедлайн:', f'День: {issue_date}, Месяц: {issue_month}')
-
-note = [
-    ('Имя пользователя:', username),
-    ('Заголовок:', [title]),
-    ('Описание:', content),
-    ('Статус:', current_status),
-    ('Дата создания:', ['День:', created_date, 'Месяц:', created_month]),
-    ('Дата дедлайна:', ['День:', issue_date, 'Месяц:', issue_month])
-]
-print('\nИнформация о заметке: ' )
-for index, about_note in enumerate(note, start=1):
-    print(f"{index}. {about_note}")
