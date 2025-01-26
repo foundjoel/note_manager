47bfacca8544b8ca52932ae46a53aa9e6a264075
foundjoel <kiseliova.marina2018@yandex.ru>
2025-01-26 17:46:42 +0300

    add_list.py

--- add_list.py
+++ add_list.py
@@ -1,17 +1,15 @@
 username = input('Введите ваше имя: ')
 print('Имя пользователя: ', username)
 
-titles = []
-while True:
-    title = input('Введите заголовок: ')
-    title2 = input('Введите заголовок: ')
-    title3 = input('Введите заголовок: ')
-    titles.append(title, title2, title3)
-print('\nСписок заголовков: ')
-for idx, title in enumerate(titles, start=1):
-    print(f"{idx}. {title}")
+title_1 = input("Введите заголовок: ")
+print(title_1)
+title_2 = input("Введите заголовок: ")
+print(title_2)
+title_3 = input("Введите заголовок: ")
+print(title_3)
+all_titles = [title_1 + ', ' + title_2 + ', ' + title_3]
+print('Список заголовков: ', all_titles)
 
-print('Заголовок заметки:', title, title2, title3)
 content = input('Введите описание заметки: ')
 print('Описание заметки: ', content)
 status = input('Введите статус заметки: ')
@@ -20,5 +18,5 @@ created_date = int(input('Введите день заметки: '))
 created_month = int(input('Введите месяц заметки: '))
 print('Дата заметки:', (f'День: {created_date}, Месяц: {created_month}'))
 issue_date = int(input('Введите дату дедлайна: '))
-issue_month = int(input('Введите месяц заметки: '))
-print('Дедлайн:', (f'День: {issue_date}, Месяц: {issue_month}'))
+issue_month = int(input('Введите месяц дедлайна: '))
+print('Дата заметки:', (f'День: {issue_date}, Месяц: {issue_month}'))
